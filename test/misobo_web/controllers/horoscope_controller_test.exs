82e1a7ae0987af2b1f63b3e6b982a61b3d6724bf
yatender-oktalk <yatender.nitk@gmail.com>
2020-10-03 19:39:21 +0530

    Adding the blank test for horoscope

--- test/misobo_web/controllers/horoscope_controller_test.exs
+++ test/misobo_web/controllers/horoscope_controller_test.exs
@@ -1,1 +1,11 @@
+defmodule MisoboWeb.HoroscopeControllerTest do
+  @moduledoc """
+  horoscope controller related tests
+  """
 
+  use MisoboWeb.ConnCase, async: true
+
+  test "horoscope controller" do
+    assert true
+  end
+end
