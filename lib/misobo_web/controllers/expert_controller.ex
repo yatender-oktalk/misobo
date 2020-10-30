d97d97c938b27fdd5889624f755c27785d9b0cff
yatender-oktalk <yatender.nitk@gmail.com>
2020-10-30 09:09:51 +0530

    Adding the data

--- lib/misobo_web/controllers/expert_controller.ex
+++ lib/misobo_web/controllers/expert_controller.ex
@@ -2,17 +2,14 @@ defmodule MisoboWeb.ExpertController do
   use MisoboWeb, :controller
 
   alias Misobo.Experts
-  # alias Misobo.Experts.Expert
-
-  # import Misobo.Commons
 
   def index(conn, _params) do
     experts = Experts.list_experts()
     response(conn, 200, experts)
   end
 
-  def fetch(conn, params) do
-    data = Experts.fetch_experts(params)
+  def fetch(conn, %{"page" => page} = _params) do
+    data = Experts.fetch_experts(page)
     response(conn, 200, Map.from_struct(data))
   end
 
