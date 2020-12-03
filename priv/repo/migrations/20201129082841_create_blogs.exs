8cf19ee5d6631138ccb75e62157b97d78a5d39a7
yatender-oktalk <yatender.nitk@gmail.com>
2020-12-03 12:39:48 +0530

    adding the image into the blog

--- priv/repo/migrations/20201129082841_create_blogs.exs
+++ priv/repo/migrations/20201129082841_create_blogs.exs
@@ -5,6 +5,7 @@ defmodule Misobo.Repo.Migrations.CreateBlogs do
     create table(:blogs) do
       add :title, :string
       add :content, :text
+      add :image, :string
       add :is_enabled, :boolean, default: true, null: false
       add :category, :string
 
